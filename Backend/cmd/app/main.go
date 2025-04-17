16da54ac5fadc1490b9ee62ee0c88586fd259961
Suryansh Rohil <suryanshrohilwork@gmail.com>
2025-04-18 03:23:29 +0530

    Deploy Try

--- Backend/cmd/app/main.go
+++ Backend/cmd/app/main.go
@@ -31,7 +31,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading config:", err)
 	}
-
+	r.Run(":" + port)
 	err = db.Connect()
 	if err != nil {
 		log.Fatal("Error connecting to database:", err)
@@ -40,7 +40,6 @@ func main() {
 	r.Use(gin.Logger(), gin.Recovery())
 
 	routes.InitRoutes(r)
-	r.Run(":" + port)
 	// r.Run(port) // Start the server on port 8080
 	db.Close()
 }
