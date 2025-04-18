94a2ec6beb47c32ef19d042491050b754c9479e8
Suryansh Rohil <suryanshrohilwork@gmail.com>
2025-04-19 04:54:14 +0530

    Deploy Try

--- Backend/cmd/app/main.go
+++ Backend/cmd/app/main.go
@@ -19,7 +19,6 @@ func main() {
 	}
 	r := gin.New() // Gin router
 	r.Use(cors.New(cors.Config{
-		// AllowOrigins: []string{"http://localhost:5173"},
 		AllowOrigins:     []string{"https://smart-city-transport.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
